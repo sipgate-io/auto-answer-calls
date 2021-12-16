73483d787990576a41145c1c5c9bd5527e77203e
Luca <schmitz@sipgate.de>
2021-12-16 16:02:55 +0100

    :sparkles: enable functionality to start a call
    Co-authored-by: Jimmy Neitzert <neitzert@sipgate.de>
    Co-authored-by: Malte Sehmer <sehmer@sipgate.de>
    Co-authored-by: Philip Kiparra <kiparra@sipgate.de>

--- pjsua-setup.sh
+++ pjsua-setup.sh
@@ -2,13 +2,17 @@
 
 ###Enter your sip credentials here###
 
-SIPID=
-SIPPASSWORD=
+export SIPID=
+export SIPPASSWORD=
+TOKEN=
+TOKENID=
+DEVICEEXTENSION=
+CALLEE=
+CALLERID=
 
 #####################################
 
 FILE=$(pwd)/pjsua-unpacked/pjsip-apps/bin
-#echo $FILE
 if [ ! -d "$FILE" ]; then
 echo "Downloading & Unpacking..."
 wget https://github.com/pjsip/pjproject/archive/2.11.1.tar.gz -O pjsua-archive.tar.gz
@@ -24,4 +28,10 @@ echo "pjsua is already installed"
 fi
 cd pjsua-unpacked
 cd pjsip-apps/bin
-./pjsua-*
+gnome-terminal  -- bash -c './pjsua-* --registrar=sip:sipgate.de --id=sip:${SIPID}@sipgate.de --realm="*" --username=$SIPID --password=$SIPPASSWORD --auto-answer=200'
+
+# Initiate Call via Post Request
+curl --location --request POST 'https://api.sipgate.com/v2/sessions/calls' \
+--header 'Content-Type: application/json' \
+--user "$TOKENID:$TOKEN" \
+--data-raw "{\"deviceId\": \"$DEVICEEXTENSION\", \"caller\": \"$DEVICEEXTENSION\", \"callee\": \"$CALLEE\", \"callerId\": \"$CALLERID\"}"
